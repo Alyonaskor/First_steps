1cf7ec7366673aef93955b8b424d838e9a358a9a
Alyona <alyona@Alyonas-MBP.lan>
2020-10-31 10:14:46 +0200

    My first git push

---
+++ replace.c
@@ -0,0 +1,22 @@
+// замена нескольких элементов строки
+#include <stdio.h>
+int main () 
+{
+char str[50] = "Replaceble characters";
+int i = 0;
+while(str[i])
+{
+
+    if (!(str[i] == 'c' && str[i + 1] == 'h'))
+        i++;
+    else 
+    {
+        str[i] = 'z';
+        str[i+1] = 'x';
+        break;
+    }
+}
+printf("%s", str);
+
+    return 0;
+}
