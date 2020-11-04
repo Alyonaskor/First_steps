ebeecb613be03980d1557347baf205118f2dc49d
Alyona <alyona@Alyonas-MBP.lan>
2020-11-04 11:38:18 +0200

    Point in Coordinate Plane

---
+++ point_inCP.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <math.h>
+int main() {
+    double x = 3;
+    double y = -1;
+    if(y > -x + 2 && x > 2 - y) 
+    printf("Area 1/n");
+    else
+    printf("Area 2");
+    return 0;
+}
